376d6d29549098fd875e1e8dd7721d84cce41da7
Levi2204 <leviteixeira225@gmail.com>
2023-11-08 14:32:56 -0300

    terceiro commit

--- PooJava/src/carro/Carro.java
+++ PooJava/src/carro/Carro.java
@@ -1,8 +1,17 @@
 package carro;
 
+import java.util.Scanner;
+
 public class Carro {
     public static void main(String[] args) {
-        classCarro meuCarro = new classCarro("Toyota", "Corolla", 2007);
+        Scanner str = new Scanner(System.in);
+        System.out.println("Digite a marca do seu carro:");
+        String marca = str.nextLine();
+        System.out.println("Digite o modelo do seu carro:");
+        String modelo = str.nextLine();
+        System.out.println("Digite o ano do seu carro:");
+        int ano = str.nextInt();
+        classCarro meuCarro = new classCarro(marca, modelo, ano);
         meuCarro.acelerar ();
         meuCarro.frear();
     }
